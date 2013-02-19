e4663306b0712a35724883be35f0e570f6d2abd3
Jurica Vukadin <u.ra604@gmail.com>
2013-02-19 16:26:45 +0100

    Fix typo in liblights

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -14,7 +14,7 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
+ifeq ($(TARGET_PROVIDES_LIBLIGHTS),true)
 ifeq ($(BOARD_VENDOR),htc)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
 
