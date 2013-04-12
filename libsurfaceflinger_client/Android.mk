c7310ae43c4bef439890145e9cf542790d322131
Jurica Vukadin <u.ra604@gmail.com>
2013-04-12 21:29:59 +0200

    Build SurfaceFlinger stub unconditionally

--- libsurfaceflinger_client/Android.mk
+++ libsurfaceflinger_client/Android.mk
@@ -1,6 +1,5 @@
 ifeq ($(BOARD_VENDOR),htc)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-ifeq ($(CAMERA_USES_SURFACEFLINGER_CLIENT_STUB),true)
 
 
 LOCAL_PATH := $(call my-dir)
@@ -14,6 +13,5 @@ LOCAL_MODULE := libsurfaceflinger_client
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif # CAMERA_USES_SURFACEFLINGER_CLIENT_STUB
 endif # TARGET_BOARD_PLATFORM
 endif # BOARD_VENDOR
