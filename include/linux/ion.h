3fb2fb83267d9357c947a0cee303d1f86a090196
poppy1 <wylhf1990@gmail.com>
2013-05-22 17:17:10 +0800

    Fix camera error

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -39,22 +39,22 @@ enum ion_heap_type {
 #define ION_HEAP_CARVEOUT_MASK (1 << ION_HEAP_TYPE_CARVEOUT)
 #define ION_HEAP_CP_MASK (1 << ION_HEAP_TYPE_CP)
 enum ion_heap_ids {
- INVALID_HEAP_ID = -1,
+ ION_HEAP_SYSTEM_ID = -1,
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- ION_HEAP_SYSTEM_ID = 1,
- ION_IOMMU_HEAP_ID = 4,
+ ION_HEAP_SYSTEM_CONTIG_ID = -1,
+ ION_HEAP_SMI_ID = -1,
+ ION_HEAP_AUDIO_ID = -1,
+ ION_HEAP_ADSP2_ID = -1,
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ INVALID_HEAP_ID = -1,
  ION_CP_MM_HEAP_ID = 8,
  ION_CP_ROTATOR_HEAP_ID = 9,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- ION_HEAP_SYSTEM_CONTIG_ID,
  ION_CP_MFC_HEAP_ID = 12,
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  ION_CP_WB_HEAP_ID = 16,
  ION_CAMERA_HEAP_ID = 20,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  ION_SF_HEAP_ID = 24,
- ION_HEAP_SMI_ID,
- ION_HEAP_AUDIO_ID,
- ION_HEAP_ADSP2_ID,
+ ION_IOMMU_HEAP_ID = 26,
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  ION_QSECOM_HEAP_ID = 27,
  ION_AUDIO_HEAP_ID = 28,
@@ -88,54 +88,55 @@ enum ion_heap_ids {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define ION_SET_CACHE(__cache) ((__cache) << ION_CACHE_SHIFT)
 #define ION_IS_CACHED(__flags) ((__flags) & (1 << ION_CACHE_SHIFT))
+#define ION_IOMMU_UNMAP_DELAYED 1
 struct ion_allocation_data {
- size_t len;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ size_t len;
  size_t align;
  unsigned int flags;
  struct ion_handle *handle;
-};
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+};
 struct ion_fd_data {
  struct ion_handle *handle;
  int fd;
-};
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+};
 struct ion_handle_data {
  struct ion_handle *handle;
 };
-struct ion_custom_data {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+struct ion_custom_data {
  unsigned int cmd;
  unsigned long arg;
 };
-struct ion_flush_data {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+struct ion_flush_data {
  struct ion_handle *handle;
  int fd;
  void *vaddr;
- unsigned int offset;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ unsigned int offset;
  unsigned int length;
 };
 struct ion_flag_data {
- struct ion_handle *handle;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ struct ion_handle *handle;
  unsigned long flags;
 };
 #define ION_IOC_MAGIC 'I'
-#define ION_IOC_ALLOC _IOWR(ION_IOC_MAGIC, 0, struct ion_allocation_data)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define ION_IOC_ALLOC _IOWR(ION_IOC_MAGIC, 0,   struct ion_allocation_data)
 #define ION_IOC_FREE _IOWR(ION_IOC_MAGIC, 1, struct ion_handle_data)
 #define ION_IOC_MAP _IOWR(ION_IOC_MAGIC, 2, struct ion_fd_data)
 #define ION_IOC_SHARE _IOWR(ION_IOC_MAGIC, 4, struct ion_fd_data)
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define ION_IOC_IMPORT _IOWR(ION_IOC_MAGIC, 5, int)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define ION_IOC_CUSTOM _IOWR(ION_IOC_MAGIC, 6, struct ion_custom_data)
-#define ION_IOC_CLEAN_CACHES _IOWR(ION_IOC_MAGIC, 7, struct ion_flush_data)
-#define ION_IOC_INV_CACHES _IOWR(ION_IOC_MAGIC, 8, struct ion_flush_data)
-#define ION_IOC_CLEAN_INV_CACHES _IOWR(ION_IOC_MAGIC, 9, struct ion_flush_data)
+#define ION_IOC_CLEAN_CACHES _IOWR(ION_IOC_MAGIC, 7,   struct ion_flush_data)
+#define ION_IOC_INV_CACHES _IOWR(ION_IOC_MAGIC, 8,   struct ion_flush_data)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define ION_IOC_GET_FLAGS _IOWR(ION_IOC_MAGIC, 10, struct ion_flag_data)
+#define ION_IOC_CLEAN_INV_CACHES _IOWR(ION_IOC_MAGIC, 9,   struct ion_flush_data)
+#define ION_IOC_GET_FLAGS _IOWR(ION_IOC_MAGIC, 10,   struct ion_flag_data)
 #endif
 
